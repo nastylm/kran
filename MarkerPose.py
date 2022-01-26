7be811bd79010b04b5da7c9ece2ab7d84c065e2f
nastylm <36473256+nastylm@users.noreply.github.com>
2022-01-26 11:39:47 +0200

    Update MarkerPose.py

--- MarkerPose.py
+++ MarkerPose.py
@@ -1,12 +1,12 @@
-# Python data structure for detected markers
-
+#параметры маркера
 class MarkerPose:
-    def __init__(self, x, y, theta, quality, order=None):
+    def __init__(self, x, y, theta, quality, order = None):
+        #координаты метки
         self.x = x
         self.y = y
         self.theta = theta
-        self.quality = quality
-        self.order = order
+        self.quality = quality #качество распознавания
+        self.order = order #номер метки, которая задана
 
     def scale_position(self, scale_factor):
         self.x = self.x * scale_factor
